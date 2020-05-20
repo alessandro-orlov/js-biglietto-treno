ecb01420ca7ef2ff5a8712ee9e089e9aa4a70ef1
Alessandro <alex.ixs@hotmail.com>
2020-05-20 14:45:47 +0200

    added some calculations logic

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,19 @@
-var userAge = prompt('Inserisci la tua età')
+// User Age
+var userAge = prompt('Inserisci la tua età');
+if (isNaN(userAge)) {
+  alert('Errore: Inserisci un valore numerico');
+}
+console.log(userAge);
 
-document.getElementById('prezzo').innerHTML = prezzoBiglietto;
+// Km to do by train
+var kmLenght = prompt('Quanti chilometri dovi percorrere');
+if (isNaN(kmLenght)) {
+  alert('Errore: inserisci un valore numerico')
+}
+console.log(kmLenght);
+
+var priceForKm = (kmLenght * 0.21)
+
+var prezzoBiglietto = priceForKm;
+
+document.getElementById('prezzo').innerHTML = prezzoBiglietto + '€';
